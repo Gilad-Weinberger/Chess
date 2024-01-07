548bc00441a92f4737ecfea8dde8aa06d78fadca
Gilad Weinberger <giladwe21@gmail.com>
2024-01-07 08:59:12 +0200

    Creating DrawBoard Function

--- Program.cs
+++ Program.cs
@@ -11,6 +11,22 @@ namespace Chess
         static void Main(string[] args)
         {
             Console.WriteLine("Let's Get Fully Started!!!");
+            DrawBoard();
+        }
+
+        public static void DrawBoard()
+        {
+            for (int rank = 1; rank <= 8; rank++)
+            {
+                Console.WriteLine(string.Concat(System.Linq.Enumerable.Repeat("-", 33))); 
+                for (int file = 1; file <= 8; file++)
+                {
+                    Console.Write("| " + "b" + " ");
+                }
+                Console.Write("|   ");
+                Console.WriteLine();
+            }
+            Console.WriteLine(string.Concat(System.Linq.Enumerable.Repeat("-", 33)));
         }
     }
 }
