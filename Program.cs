9f6b9dd60b6685f2846b2ee534cd4a5e7f31701f
Gilad Weinberger <giladwe21@gmail.com>
2024-01-07 10:38:45 +0200

    start LoadFromFen function

--- Program.cs
+++ Program.cs
@@ -29,5 +29,37 @@ namespace Chess
             Console.WriteLine(string.Concat(System.Linq.Enumerable.Repeat("-", 33)));
 
         }
+
+        public static void LoadPositionFromFen(string fen)
+        {
+            var pieceTypeFromSymbol = new Dictionary<char, int>()
+            {
+                ['k'] = Piece.King,
+                ['p'] = Piece.Pawn,
+                ['n'] = Piece.Knight,
+                ['b'] = Piece.Bishop,
+                ['r'] = Piece.Rook,
+                ['q'] = Piece.Queen
+            };
+
+            string fenBoard = fen.Split(' ')[0];
+            int file = 0, rank = 7;
+
+            foreach (char symbol in fenBoard)
+            {
+                if (symbol == '/')
+                {
+                    file = 0;
+                    rank--;
+                }
+                else
+                {
+                    if (char.IsDigit(symbol))
+                    {
+                        file += (int)char.GetNumericValue(symbol);
+                    }
+                }
+            }
+        }
     }
 }
