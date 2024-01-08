483eab6d59c18af866d443a9d573293d66ee3f89
Gilad Weinberger <giladwe21@gmail.com>
2024-01-08 22:50:04 +0200

    Commit

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ namespace Chess
         {
             const string startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
             int[] DirectionOffsets = { 8, -8, 1, -1, 7, -7, 9, -9};
+            int[][] NumSquaresToEdge = new int[8][];
+            NumSquaresToEdge = PreComputedMoveData(NumSquaresToEdge);
 
             Board Board = new Board();
             Board.LoadPositionFromFen(startFen);
@@ -33,6 +35,34 @@ namespace Chess
             }
             Console.WriteLine(string.Concat(Enumerable.Repeat("-", 33)));
             Console.WriteLine();
+        }
+
+        static int[][] PreComputedMoveData(int[][] numSquaresToEdge)
+        {
+            for (int file = 0; file < 8; file++)
+            {
+                for (int rank = 0; rank < 8; rank++)
+                {
+                    int numTop = 7 - rank;
+                    int numDown = rank;
+                    int numLeft = file;
+                    int numRight = 7 - file;
+
+                    int squareIndex = rank * 8 + file;
+
+                    numSquaresToEdge[squareIndex] = new int[] {
+                        numTop,
+                        numDown,
+                        numLeft,
+                        numRight,
+                        Math.Min(numTop, numLeft),
+                        Math.Min(numDown, numRight),
+                        Math.Min(numTop, numRight),
+                        Math.Min(numDown, numLeft)
+                    };
+                }
+            }
+            return numSquaresToEdge;
         }
     }
 }
